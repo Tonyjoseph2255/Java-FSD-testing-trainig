debac0bfb91f75389c364e12c3b0199de8110d92
Tonyjoseph2255 <101250944+Tonyjoseph2255@users.noreply.github.com>
2022-03-31 10:23:47 +0530

    Add files via upload

---
+++ src/Demo.java
@@ -0,0 +1,21 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int a=10;
+		int[] arr=new int[5];//array decleration
+		int [] arr1= {1,2,22,23,45};
+		int[ ] arr2=new int[4];
+		arr2[0]=254;
+		arr2[1]=24;
+		arr2[2]=1254;
+		arr2[3]=888;
+		System.out.println(arr2[2]);
+		String[] countrynames=new String[206];
+		countrynames[0]="India";
+		String []countrynames1= {"India,China,Brazil"};
+		System.out.println(countrynames[2]);
+		System.out.println(countrynames.length);
+	}
+
+}
---
+++ src/demo/Demo2.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		int a=5,sum=0;
+		while(a>0) {
+			 sum=sum+a;
+			a--;
+		}System.out.println(sum);
+	}
+
+}
